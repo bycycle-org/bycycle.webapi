484c58b1db9663c1812adfbb84dd959f7570ac1a
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2012-11-14 16:09:14 -0800

    Clean up setup.py.
    - Remove big, crufty comment header
    - Fix package name (byCycleTripPlanner => bycycle.tripplanner)
    - Fix version (0.5 => 0.5.dev0; zest.releaser will be used to manage version
      numbers from here on out)
    - Clarify GPL is v3
    - Change download URL to point at Bitbucket
    - Remove superfluous trove classifiers
    - Indicate supported Python version is 2.7

--- setup.py
+++ setup.py
@@ -1,41 +1,25 @@
-###############################################################################
-# $Id: setup.py 459 2007-02-15 12:05:14Z bycycle $
-# Created 2006-09-07
-#
-# Project setup for byCycle Trip Planner.
-#
-# Copyright (C) 2006-2008 Wyatt Baldwin, byCycle.org <wyatt@bycycle.org>.
-# All rights reserved.
-#
-# For terms of use and warranty details, please see the LICENSE file included
-# in the top level of this distribution. This software is provided AS IS with
-# NO WARRANTY OF ANY KIND.
-###############################################################################
 from setuptools import setup, find_packages
 
+
 setup(
-    name='byCycleTripPlanner',
-    version='0.5',
+    name='bycycle.tripplanner',
+    version='0.5.dev0',
     description='byCycle Trip Planner',
     long_description='byCycle Bicycle Trip Planner Web Application',
-    license='GNU General Public License (GPL)',
+    license='GPLv3',
     author='Wyatt L Baldwin, byCycle.org',
     author_email='wyatt@bycycle.org',
     keywords='bicycle bike cycyle trip planner route finder',
     url='http://bycycle.org/',
-    download_url='http://guest:guest@code.bycycle.org/TripPlanner/trunk#egg=byCycleTripPlanner-dev',
-    classifiers=[
+    download_url='https://bitbucket.org/wyatt/bycycle.core',
+    classifiers=(
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Pylons',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Topic :: Education',
-        'Topic :: Scientific/Engineering :: GIS',
-    ],
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+    ),
     packages=find_packages(),
     zip_safe=False,
     install_requires=(
