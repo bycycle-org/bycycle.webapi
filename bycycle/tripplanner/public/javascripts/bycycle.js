0dca66e77b23147f27d092bd9070d479ba71b3c9
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2012-11-14 23:05:37 -0800

    Clean up bycycle JS module a bit.
    - A bunch of little cleanups like `typeof(x) == y` => `typeof x === y`
    - Whitespace cleanup
    - Improved and corrected comments throughout
    - byCycle.noop (a do-nothing function) was removed because it was only used
      in this one module and was only used once

--- bycycle/tripplanner/public/javascripts/bycycle.js
+++ bycycle/tripplanner/public/javascripts/bycycle.js
@@ -1,47 +1,39 @@
-/**
- * byCycle namespace
- */
 var byCycle = (function() {
-  // private:
-  var prod_config = {
-    local: 0,
-    map_type: 'google',
-    map_state: 1
+  var config = {
+    prod: {
+      local: 0,
+      map_type: 'google',
+      map_state: 1
+    },
+    dev: {
+      local: 1,
+      map_type: 'base',
+      map_state: 1
+    }
   };
-
-  var dev_config = {
-    local: 1,
-    map_type: 'base',
-    map_state: 1
-  };
-
-  var noop = function() {};
 
   var console_debug = function() {
-	console.debug.apply(console, arguments);
+   console.debug.apply(console, arguments);
   }
 
   var hostname = location.hostname;
   var port = location.port;
 
-  // public:
   return {
     // `debug` is a global set in the template; it's value is passed from
-    // Pylons as an attribute of the global `g`
-    config: debug ? dev_config : prod_config,
+    // Pylons as an attribute of `app_globals`
+    config: debug ? config.dev : config.prod,
 
-    // Used to look Google API key in gmap.js and to make queries in ui.js
+    // Used to look up Google API key in gmap.js and to make queries in ui.js
     domain: (port ? [hostname, port].join(':') : hostname),
 
-    // Prefix for when app is mounted at other than root (/)
+    // Prefix for when app is mounted somewhere other than root (/)
     prefix: byCycle_prefix,
 
     // URL query parameters as a Hash
     request_params: $H(location.search.toQueryParams()),
 
     default_map_type: 'base',
-
-    noop: noop,
 
     // Namespace for byCycle widgets
     widget: {},
@@ -53,8 +45,8 @@ var byCycle = (function() {
     getParamVal: function(var_name, func) {
       // Override config setting with query string setting
       var v = byCycle.request_params[var_name];
-      if (typeof(v) == 'undefined') {
-	// Query string override not given; use config
+      if (typeof v === 'undefined') {
+      // Query string override not given; use config
         v = byCycle.config[var_name];
       } else if (typeof(func) == 'function') {
         // Process query string value with func, iff given
@@ -63,6 +55,6 @@ var byCycle = (function() {
       return v;
     },
 
-    logDebug: (debug ? console_debug : noop)
+    logDebug: (debug ? console_debug : function () {})
   };
 })();
