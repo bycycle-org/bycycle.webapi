ab197823778d48097b0183790eb91167dd6c2b91
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2012-12-02 23:00:37 -0800

    Clean up bycycle.js
    - Remove `local` config option (not sure what it was for)
    - Remove `byCycle.domain` property (no longer used)
    - Reformat some comments
    - Change `typeof()` to `typeof `
    - Move () to call anon namespace func inside parens (recommended by
      jslint)

--- bycycle/tripplanner/static/javascripts/bycycle.js
+++ bycycle/tripplanner/static/javascripts/bycycle.js
@@ -1,27 +1,19 @@
-var byCycle = (function() {
+var byCycle = (function () {
   var config = {
     prod: {
-      local: 0,
       map_type: 'google',
       map_state: 1
     },
     dev: {
-      local: 1,
       map_type: 'base',
       map_state: 1
     }
   };
 
-  var hostname = location.hostname;
-  var port = location.port;
-
   return {
-    // `debug` is a global set in the template; it's value is passed from
-    // Pylons as an attribute of `app_globals`
+    // `debug` is a global set in the template; it's value is passed
+    // from Pylons as an attribute of `app_globals`
     config: debug ? config.dev : config.prod,
-
-    // Used to look up Google API key in gmap.js and to make queries in ui.js
-    domain: (port ? [hostname, port].join(':') : hostname),
 
     // Prefix for when app is mounted somewhere other than root (/)
     prefix: byCycle_prefix,
@@ -35,20 +27,20 @@ var byCycle = (function() {
     widget: {},
 
     /**
-     * Get value for variable from query string if possible, otherwise use the
-     * global config value
+     * Get value for variable from query string if possible;
+     * otherwise use the global config value
      */
     getParamVal: function(var_name, func) {
       // Override config setting with query string setting
       var v = byCycle.request_params.get(var_name);
       if (typeof v === 'undefined') {
-      // Query string override not given; use config
+        // Query string override not given; use config
         v = byCycle.config[var_name];
-      } else if (typeof(func) == 'function') {
+      } else if (typeof func === 'function') {
         // Process query string value with func, iff given
         v = func(v);
       }
       return v;
     }
   };
-})();
+}());
