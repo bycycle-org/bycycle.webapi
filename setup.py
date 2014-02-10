7a264a24cc45018c82748dcd89d616827f88c9c3
Wyatt <self@wyattbaldwin.com>
2014-02-09 17:45:27 -0800

    Switch to released versions of all tangled.* packages

--- setup.py
+++ setup.py
@@ -12,30 +12,30 @@ setup(
     keywords='bicycle bike cycle trip planner route finder',
     url='http://bycycle.org/',
     download_url='https://bitbucket.org/wyatt/bycycle.core',
-    classifiers=(
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.3',
-    ),
+    ],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=(
+    install_requires=[
         'bycycle.core>=0.5a2',
-        'tangled.mako',
-        'tangled.sqlalchemy',
-        'tangled.web',
-    ),
-    extras_require=dict(
-        dev=(
-            'tangled.web[dev]'
-        ),
-        test=(
+        'tangled.mako>=0.1a2',
+        'tangled.sqlalchemy>=0.1a2',
+        'tangled.web>=0.1a5',
+    ],
+    extras_require={
+        'dev': [
+            'tangled.web[dev]>=0.1a5',
+        ],
+        'test': [
             'coverage>=3.7',
             'WebTest>=2.0.10',
-        ),
-    ),
+        ],
+    },
     test_suite='bycycle.tripplanner',
 )
