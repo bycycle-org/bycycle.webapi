19929b60f35cc08096d3009236b1a5682e16ba36
Wyatt Baldwin <self@wyattbaldwin.com>
2019-05-30 18:46:40 -0700

    Improve query service facade wrt. error handling
    - Raise error when no query parameters are passed
    - Raise error when both `term` and either `from` or `to` query
      parameters are passed
    - Let selected service handle blank parameter values
    - Handle raised errors the same way as other services (convert to dict
      and set response status to 400)

--- bycycle/webapi/views/query.py
+++ bycycle/webapi/views/query.py
@@ -1,6 +1,6 @@
 import re
 
-from pyramid.httpexceptions import exception_response
+from bycycle.core.exc import InputError
 
 from .directions import DirectionsResource
 from .lookup import LookupResource
@@ -13,25 +13,37 @@ DIRECTIONS_RE = re.compile(r'.+\s+to\s+.+')
 class QueryResource(ServiceResource):
 
     def get(self):
+        try:
+            return self._get()
+        except InputError as exc:
+            self.request.response.status_int = 400
+            return {
+                'service': 'query',
+                'error': self._exc_as_dict(exc)
+            }
+
+    def _get(self):
         req = self.request
         params = req.params
+        has_directions_params = 'from' in params or 'to' in params
 
-        if not params:
-            return {}
-
-        term = params.get('term', '').strip()
-        from_ = params.get('from', '').strip()
-        to = params.get('to', '').strip()
-
-        if term:
+        if 'term'in params:
+            if has_directions_params:
+                self.request.response.status_int = 400
+                raise InputError(
+                    'Query service accepts *either* the `term` query parameter *or* the `from` '
+                    'and `to` query parameters')
+            term = params.get('term', '').strip()
             if DIRECTIONS_RE.match(term):
                 view_class = DirectionsResource
             else:
                 view_class = LookupResource
-        elif from_ or to:
+        elif has_directions_params:
             view_class = DirectionsResource
         else:
-            raise exception_response(400)
+            raise InputError(
+                'Query service requires at least one of the following query parameter: '
+                '`term`, `from`, or `to`')
 
         view = view_class(self.request)
         return view.get()
